57620398f287fe1cbc676a6b71a1b5ff2aeb64d1
Ravi Chandra Ravulapati <ravi.ravulapati@talktalkplc.com>
2016-03-19 18:20:21 +0000

    Updated rooms json

--- hotel_v1.0_master/customer_site/modules/rooms/scripts/services/services.js
+++ hotel_v1.0_master/customer_site/modules/rooms/scripts/services/services.js
@@ -2,7 +2,7 @@
 define(['app'], function(app){
     app.factory('rooms', ['$resource',
         function($resource){
-            return $resource('https://github.com/Fyrd/caniuse/blob/master/data.json', {}, {
+            return $resource('modules/rooms/jsons/rooms.json', {}, {
             get: {method:'GET', params:{}, isArray:true}
         });
     }]);
